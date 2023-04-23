4bcd59420de774576b73941ffa16398e6c0266d3
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-22 19:52:18 -0500

    completed labwork

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentences = [phrase + '!' for phrase in sentence_list]
+    return new_sentences
